43171d3a53d2ac5343c73b4871eb21413719de3d
Uriel Ofir <urielofir@gmail.com>
2023-12-07 17:33:41 +0200

    Better backend

---
+++ example/backend/elevator.ts
@@ -0,0 +1,92 @@
+import FSM from "../../src/fsm";
+import { EventEmitter } from "events";
+const eventEmitter = new EventEmitter();
+
+let currentFloor = 0;
+let targetFloor = 0;
+let callingFloor = 0;
+
+let elevatorFSM = new FSM("idle");
+
+// Define states
+elevatorFSM.defineState("idle");
+elevatorFSM.defineState("moving");
+elevatorFSM.defineState("gotToFloor");
+
+// Define transitions
+elevatorFSM.defineTransition(
+  "idle",
+  "moving",
+  "moveToCallingFloor",
+  moveToCallingFloor
+);
+elevatorFSM.defineTransition(
+  "moving",
+  "gotToFloor",
+  "arriveAtCallingFloor",
+  arriveAtCallingFloor
+);
+elevatorFSM.defineTransition(
+  "gotToFloor",
+  "moving",
+  "moveToTargetFloor",
+  moveToTargetFloor
+);
+elevatorFSM.defineTransition(
+  "moving",
+  "gotToFloor",
+  "arriveAtTargetFloor",
+  arriveAtTargetFloor
+);
+elevatorFSM.defineTransition(
+  "gotToFloor",
+  "idle",
+  "returnToIdle",
+  returnToIdle
+);
+
+function moveToCallingFloor() {
+  eventEmitter.emit(
+    "updateClient",
+    `Moving from floor ${currentFloor} to calling floor ${callingFloor}`
+  );
+  currentFloor = callingFloor;
+  elevatorFSM.transition("arriveAtCallingFloor");
+}
+
+function arriveAtCallingFloor() {
+  eventEmitter.emit("updateClient", `Arrived at calling floor ${callingFloor}`);
+  elevatorFSM.transition("moveToTargetFloor");
+}
+
+function moveToTargetFloor() {
+  eventEmitter.emit(
+    "updateClient",
+    `Moving from floor ${currentFloor} to target floor ${targetFloor}`
+  );
+  currentFloor = targetFloor;
+  elevatorFSM.transition("arriveAtTargetFloor");
+}
+
+function arriveAtTargetFloor() {
+  eventEmitter.emit("updateClient", `Arrived at target floor ${targetFloor}`);
+  elevatorFSM.transition("returnToIdle");
+}
+
+function returnToIdle() {
+  eventEmitter.emit("updateClient", `Returning to idle state`);
+}
+
+const goToFloor = (call, target) => {
+  callingFloor = call;
+  targetFloor = target;
+  elevatorFSM.transition("moveToCallingFloor");
+};
+
+function updateClient(ws) {
+  eventEmitter.on("updateClient", (data) => {
+    ws.send(data);
+  });
+}
+
+export { elevatorFSM, goToFloor, updateClient };
--- example/backend/server.ts
+++ example/backend/server.ts
@@ -1,67 +1,35 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import FSM from '../../src/fsm';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import WebSocket from 'ws';
+
+import { elevatorFSM, goToFloor, updateClient } from "./elevator";
+
 
 const app = express();
 app.use(cors());
 
-let currentFloor = 0;
-let targetFloor = 0;
-let callingFloor = 0;
-
-let elevatorFSM = new FSM("idle");
-
-// Define states
-elevatorFSM.defineState("idle");
-elevatorFSM.defineState("moving");
-elevatorFSM.defineState("gotToFloor");
-
-// Define transitions
-elevatorFSM.defineTransition("idle", "moving", "moveToCallingFloor", moveToCallingFloor);
-elevatorFSM.defineTransition("moving", "gotToFloor", "arriveAtCallingFloor", arriveAtCallingFloor);
-elevatorFSM.defineTransition("gotToFloor", "moving", "moveToTargetFloor", moveToTargetFloor);
-elevatorFSM.defineTransition("moving", "gotToFloor", "arriveAtTargetFloor", arriveAtTargetFloor);
-elevatorFSM.defineTransition("gotToFloor", "idle", "returnToIdle", returnToIdle);
-
-function moveToCallingFloor() {
-  console.log(`Moving from floor ${currentFloor} to calling floor ${callingFloor}`);
-  currentFloor = callingFloor;
-  elevatorFSM.transition("arriveAtCallingFloor");
-}
-
-function arriveAtCallingFloor() {
-  console.log(`Arrived at calling floor ${callingFloor}`);
-  elevatorFSM.transition("moveToTargetFloor");
-}
-
-function moveToTargetFloor() {
-  console.log(`Moving from floor ${currentFloor} to target floor ${targetFloor}`);
-  currentFloor = targetFloor;
-  elevatorFSM.transition("arriveAtTargetFloor");
-}
-
-function arriveAtTargetFloor() {
-  console.log(`Arrived at target floor ${targetFloor}`);
-  elevatorFSM.transition("returnToIdle");
-}
-
-function returnToIdle() {
-  console.log(`Returning to idle state`);
-}
-
-app.get('/state', (req: Request, res: Response) => {
+app.get("/state", (req: Request, res: Response) => {
   res.send({ state: elevatorFSM.getState() });
 });
 
 app.use(express.json());
 
-app.post('/goToFloor', (req: Request, res: Response) => {
-  ({ targetFloor, callingFloor } = req.body);
-  if (elevatorFSM.getState() === "idle") {
-    elevatorFSM.transition("moveToCallingFloor");
-  }  res.send({ state: elevatorFSM.getState() });
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('Received:', message);
+  });
+  updateClient(ws);
+});
+
+app.post("/goToFloor", (req: Request, res: Response) => {
+  const { callingFloor, targetFloor } = req.body;
+  goToFloor(callingFloor, targetFloor);
+  res.send({ state: elevatorFSM.getState() });
 });
 
 app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+  console.log("Server is running on port 3001");
 });
+
