eba6d6e904240758a3fcd5be200ef59dc38555df
Uriel Ofir <urielofir@gmail.com>
2023-12-09 10:58:47 +0200

    clear warnings

--- example/frontend/src/components/Building.tsx
+++ example/frontend/src/components/Building.tsx
@@ -1,10 +1,4 @@
-import React, { useState, useEffect } from "react";
 import Floor from './Floor';
-
-interface Level {
-  id: number;
-  name: string;
-}
 
 interface BuildingProps {
     isDoorOpen: boolean;
@@ -14,18 +8,8 @@ interface BuildingProps {
   }
 
 const Building: React.FC<BuildingProps> = ({elevatorCall, elevatorState, isDoorOpen, currentFloor}) => {
-  const [state, setState] = useState<string | null>(null);
-  const levelsArray: Level[] = [
-    { id: 1, name: "1" },
-    { id: 2, name: "2" },
-    { id: 3, name: "3" },
-    { id: 4, name: "4" },
-    { id: 5, name: "5" },
-  ];
 
   const floorsAmount = 5;
-  useEffect(() => {
-  }, []);
 
   return (
         <div className="building">
